66f5fd1eb65a9f0e689ef4e7f89d0423e9221908
bp <bartek.paraf@gmail.com>
2023-06-04 21:59:55 +0200

    added download ways for later reference

--- Scripts/yt-download.sh
+++ Scripts/yt-download.sh
@@ -1,2 +1,8 @@
+# only Audio
 yt-dlp -f 'ba' https://www.youtube.com/watch?v=dQw4w9WgXcQ -o '%(id)s.%(ext)s'
 
+#Audio and Video, best quality
+yt-dlp -f 'bv*+ba' https://www.youtube.com/watch?v=dQw4w9WgXcQ -o '%(id)s.%(ext)s'
+
+#Audio and Video, 720p
+yt-dlp -f 'bv*[height=720]+ba' https://www.youtube.com/watch?v=dQw4w9WgXcQ -o '%(id)s.%(ext)s'
