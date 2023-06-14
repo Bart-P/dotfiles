4996d4f78cca04b5cca769dcaac8d7e1cc0ff6dd
bp <bartek.paraf@gmail.com>
2023-06-14 19:09:49 +0200

    org-level faces are now loaded after org-faces
    an error was throughn on startup, googled solution seem to have worked well - it basically first loads org-faces then sets the org level face styles

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -172,25 +172,25 @@
   :hook (org-mode . org-bullets-mode)
   :custom
   (org-bullets-bullet-list '("●" "◉" "○" "◉" "○" "◉" "○" "◉")))
+(with-eval-after-load 'org-faces
+    (dolist (face '((org-level-1 . 1.2)
+		    (org-level-2 . 1.1)
+		    (org-level-3 . 1.05)
+		    (org-level-4 . 1.0)
+		    (org-level-5 . 1.0)
+		    (org-level-6 . 1.0)
+		    (org-level-7 . 1.0)
+		    (org-level-8 . 1.0)))
+    (set-face-attribute (car face) nil :font "Ubuntu Nerd Font" :weight 'book :height (cdr face))
 
-(dolist (face '((org-level-1 . 1.2)
-	        (org-level-2 . 1.1)
-	        (org-level-3 . 1.05)
-	        (org-level-4 . 1.0)
-	        (org-level-5 . 1.0)
-	        (org-level-6 . 1.0)
-	        (org-level-7 . 1.0)
-	        (org-level-8 . 1.0)))
-  (set-face-attribute (car face) nil :font "Ubuntu Nerd Font" :weight 'medium :height (cdr face))
-
-    ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-    (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+	;; Ensure that anything that should be fixed-pitch in Org files appears that way
+	(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+	(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+	(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+	(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)))
 
 (defun efs/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
