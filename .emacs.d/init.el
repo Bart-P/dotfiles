d92c91ea857f42408dc084ff8ea4bcfd0377b6c5
bp <bartek.paraf@gmail.com>
2023-06-26 20:44:14 +0200

    fixed typo which broke the config...

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -98,7 +98,7 @@
 (unless (package-installed-p 'use-package)
    (package-install 'use-package))
 
-(Require 'use-package)
+(require 'use-package)
 (setq use-package-always-ensure t)
 
 (use-package swiper)
