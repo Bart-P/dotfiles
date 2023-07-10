9b83c426b9e79b7f965a89a0812be8d91be111b3
bp <bartek.paraf@gmail.com>
2023-07-10 09:31:02 +0200

    fonts adjusted - again...

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,7 +37,7 @@
 
 (set-face-attribute 'default nil
                   :font "JetBrainsMono Nerd Font Mono"
-                  :height 120
+                  :height 130
                   :weight 'regular)
 
 (set-face-attribute 'variable-pitch nil
@@ -47,7 +47,7 @@
 
 (set-face-attribute 'fixed-pitch nil
                   :font "JetBrainsMono Nerd Font Mono"
-                  :height 120
+                  :height 130
                   :weight 'regular)
 
 (use-package doom-themes
