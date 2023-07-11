e3d30a7bd05ef73c867e75aea5ff79676bd152d8
bp <bartek.paraf@gmail.com>
2023-07-11 20:57:17 +0200

    removed unused bindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -145,9 +145,7 @@
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history)))
+	 ("C-x C-f" . counsel-find-file)))
 
 ;; use evil mode EVERYWHERE
 (use-package evil
